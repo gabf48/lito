198c1f37589baa920f993dc2cf0d14650186a1c6
gabriel <gabf48@gmail.com>
2023-05-04 10:54:16 +0300

    scroll at Adauga in cos button

--- pages/cauta_hotel.py
+++ pages/cauta_hotel.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
+BUTTON_CAUTARE = 'cautare-btn1'
+
 class CautaHotelPage:
 
     def __init__(self, driver):
@@ -10,3 +12,6 @@ class CautaHotelPage:
         open_dropdown = self.driver.find_element(By.ID,'select2-fieldStatiune-container').click()
         type_statiune_name = self.driver.find_element(By.CSS_SELECTOR, '[type="search"]').send_keys(nume_statiune)
         type_statiune_name = self.driver.find_element(By.CSS_SELECTOR, '[type="search"]').send_keys(Keys.ENTER)
+
+    def apasa_cautare(self):
+        self.driver.find_element(By.ID,BUTTON_CAUTARE).click()
--- tests/adauga.py
+++ tests/adauga.py
@@ -1,6 +1,8 @@
 import time
 
 from selenium import webdriver
+
+from pages.handle_rezervare import HandleRezervare
 from pages.login_page import LoginPage
 from pages.cauta_hotel import CautaHotelPage
 
@@ -11,9 +13,13 @@ def test_adauga_in_cos():
 
     login_page = LoginPage(driver)
     cauta_hotel = CautaHotelPage(driver)
+    handle_rezervare = HandleRezervare(driver)
 
     login_page.login('https://www.stgtourop.litoralulromanesc.ro/','wesrom','lsajkacs2osj')
     time.sleep(5)
     cauta_hotel.selecteaza_statiune('Mamaia')
+    cauta_hotel.apasa_cautare()
+    time.sleep(15)
 
+    handle_rezervare.apasa_adaugare_in_cos()
     time.sleep(15)
